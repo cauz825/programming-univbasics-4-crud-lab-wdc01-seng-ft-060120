0a0ee443736efd6bf14272c5f99c8c926e6acbfd
Michael Causey <michaelhcausey@gmail.com>
2020-05-16 22:16:26 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,8 +7,7 @@ artists = ["hardy", "cole swindell", "dierks bentley", "luke bryan"]
 end
 
 def add_element_to_end_of_array(array, element)
-  artists.push("niko moon")
-  p artists
+  
 end
 
 def add_element_to_start_of_array(array, element)
