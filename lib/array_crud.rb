93cacd346cf4d571999f355d2d5c65fcb5411853
Michael Causey <michaelhcausey@gmail.com>
2020-05-16 22:20:53 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,7 +8,7 @@ end
 
 def add_element_to_end_of_array(array, element)
   artists = ["hardy", "cole swindell", "dierks bentley"]
-  artists << "niko moon"
+  artists.push("niko moon")
   p artists
 end
 
